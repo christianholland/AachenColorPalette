f31c3348952da6ba701619cc919f4daa01885a63
Christian <cholland2408@gmail.com>
2019-10-22 20:30:00 +0200

    script to make figure of color palette

--- data-raw/make_color_palettes.R
+++ data-raw/make_color_palettes.R
@@ -3,3 +3,7 @@ rwth_colors_df = read_csv("data-raw/rwth_colors.csv") %>%
                            intensity != 100 ~ paste0(color, intensity)))
 
 devtools::use_data(rwth_colors_df, internal = T, overwrite = T)
+
+# create figure for README
+palette = display_rwth_colors()
+ggsave("man/figures/rwth_color_palette.png", palette, width = 4, height=6)
